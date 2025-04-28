3551c0feea879d3c9e3a021bece573500e058e38
Manish Chaudhary <manishchaudhary3803@gmail.com>
2025-04-28 16:40:45 +0530

    second

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,2 @@
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://stack-wave-backend.onrender.com"
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://stack-wave-backend-w67j.onrender.com"
